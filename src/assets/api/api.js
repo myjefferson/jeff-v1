4dd67b5ab1db9de48ccc8b4e6dae66604f9fb097
Jefferson Carvalho <jcsjeffrey@gmail.com>
2025-10-31 22:08:25 -0300

    fix api.js

--- src/assets/api/api.js
+++ src/assets/api/api.js
@@ -2,14 +2,12 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'https://www.handgeev.com',
+  withCredentials: false,
 });
-api.use(cors({
-    origin: 'https://www.handgeev.com',
-    methods: ['GET'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
+
 const primaryHash = import.meta.env.VITE_WORKSPACE_HASH_API;
 api.defaults.headers.common['Authorization'] = `Bearer ${primaryHash}`;
+api.defaults.headers.common['Content-Type'] = 'application/json';
 
 export const apiTopicProject = () => api.get('/api/topics/3/fields');
 export const apiTopicSkill = () => api.get('/api/topics/7/fields');
