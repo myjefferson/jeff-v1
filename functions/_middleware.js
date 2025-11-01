1cc60ec673d6137780049a555ab1a5e0fb77d07c
Jefferson Carvalho <jcsjeffrey@gmail.com>
2025-10-31 23:44:26 -0300

    allow all domains

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -3,7 +3,7 @@ export async function onRequest({ request, next }) {
   
   // Adiciona headers CORS para todas as respostas
   response.headers.set('Access-Control-Allow-Origin', '*');
-  response.headers.set('Access-Control-Allow-Methods', 'GET');
+  response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   
   return response;
