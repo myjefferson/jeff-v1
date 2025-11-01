c7497ea2738f9b6ab47c110f92c411793d064d38
Jefferson Carvalho <jcsjeffrey@gmail.com>
2025-10-31 23:57:07 -0300

    update api.js

--- src/assets/api/api.js
+++ src/assets/api/api.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://www.handgeev.com/api',
+  baseURL: 'https://www.handgeev.com/api', // Já tem /api aqui
 });
 
 const primaryHash = import.meta.env.VITE_WORKSPACE_HASH_API;
 api.defaults.headers.common['Authorization'] = `Bearer ${primaryHash}`;
 api.defaults.headers.common['Content-Type'] = 'application/json';
 
-export const apiTopicProject = () => api.get('/api/topics/3/fields');
-export const apiTopicSkill = () => api.get('/api/topics/7/fields');
-export const apiTopicProfile = () => api.get('/api/topics/8/fields');
+export const apiTopicProject = () => api.get('/topics/3/fields');
+export const apiTopicSkill = () => api.get('/topics/7/fields');
+export const apiTopicProfile = () => api.get('/topics/8/fields');
